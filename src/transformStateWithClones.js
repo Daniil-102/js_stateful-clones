a848e25827e814d5a14b12788c211ff16e03257a
Daniel Kononchuk <11fm2020arli@gmail.com>
2024-10-29 20:38:00 +0200

    resolve task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let newState = { ...state };
+  const res = [];
+
+  for (const action of actions) {
+    const { type } = action;
+
+    if (type === 'addProperties') {
+      const { extraData: data } = action;
+
+      newState = { ...newState, ...data };
+    }
+
+    if (type === 'removeProperties') {
+      const { keysToRemove: data } = action;
+
+      for (const key of data) {
+        delete newState[key];
+      }
+    }
+
+    if (type === 'clear') {
+      newState = {};
+    }
+
+    res.push({ ...newState });
+  }
+
+  return res;
 }
 
 module.exports = transformStateWithClones;
